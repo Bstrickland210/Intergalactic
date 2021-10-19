928c02c34c7035d6c56a3f7a5bae54040762f8d6
Bridger Strickland <bridger210strick@gmail.com>
2021-10-19 09:29:39 -0600

    Initial Commit

---
+++ Intergalactic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intergalactic
+//
+//  Created by Bridger Strickland on 10/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
